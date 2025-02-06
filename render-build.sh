f3b218e3fcf3e4aab1d3aacfcc1a02aeac2e6659
TatsatJha <TatsatTarun@gmail.com>
2025-02-06 13:59:32 -0700

    update version of hugo in build script

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,13 @@
 # save as render-build.sh and make sure it's executable
+# update the version being used to match version being used in build
 hugo version # Output the OLD version
 if [[! -f $XDG_CACHE_HOME/hugo]]; then 
   echo "...Downloading HUGO" 
   mkdir -p ~/tmp 
-  wget -P ~/tmp https://github.com/gohugoio/hugo/releases/download/v0.99.1/hugo_0.99.1_Linux-32bit.tar.gz 
+  wget -P ~/tmp https://github.com/gohugoio/hugo/releases/download/v0.142.0/hugo_0.142.0_Linux-64bit.tar.gz 
   cd ~/tmp
   echo "...Extracting HUGO" 
-  tar -xzvf hugo_0.99.1_Linux-32bit.tar.gz
+  tar -xzvf hugo_0.142.0_Linux-64bit.tar.gz
   echo "...Moving HUGO"
   mv hugo $XDG_CACHE_HOME/hugo  
   cd $HOME/project/src # Make sure we return to where we were
